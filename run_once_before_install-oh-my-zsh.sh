811ce4d507dd302dc4e72509e63b88047389c98f
Marc Fernandez <marc.fernandezg@gmail.com>
2023-06-04 16:56:26 -0700

    Check if Oh My ZSH is already installed

--- run_once_before_install-oh-my-zsh.sh
+++ run_once_before_install-oh-my-zsh.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+FILE=$HOME/.oh-my-zsh/oh-my-zsh.sh
+if [ -f "$FILE" ]; then
+  echo "oh-my-zsh already installed."
+else
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
